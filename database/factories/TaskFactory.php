7daf11b0a0f7037b2443e5063614948b153a31ec
VVoronkov <voronglass84@gmail.com>
2026-01-13 23:45:49 +0300

    Add seed for Task model

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    protected $model = Task::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->text,
+            'status' => $this->faker->randomElement(['new', 'done', 'active']),
+        ];
+    }
+}
