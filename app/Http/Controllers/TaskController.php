37e68fee521c617f6f8d057d263952636ad4228b
VVoronkov <voronglass84@gmail.com>
2026-01-13 20:42:50 +0300

    Add store method

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,12 +8,26 @@ use Illuminate\Http\Request;
 class TaskController extends Controller
 {
     //GET /tasks
-    public function index() {
+    public function index()
+    {
         return Task::all();
     }
 
     //GET /tasks/{id}
-    public function show($id) {
+    public function show($id)
+    {
         return Task::findOrFail($id);
+    }
+
+    //POST /tasks
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string',
+            'description' => 'nullable|string',
+            'status' => 'required|string',
+        ]);
+
+        return Task::create($validated);
     }
 }
