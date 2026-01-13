614473a47ccadfc61acf794145df354d58450a9a
VVoronkov <voronglass84@gmail.com>
2026-01-13 20:28:35 +0300

    Create TaskController with index method

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    //GET /index
+    public function index() {
+        return Task::all();
+    }
+}
